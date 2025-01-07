1ea080a056ecdf1466e8cb97786eea561f688b2b
sraj9506 <154707773+sraj9506@users.noreply.github.com>
2025-01-07 11:18:55 +0530

    Update SignUp.jsx

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -26,7 +26,7 @@ const SignUp = () => {
     }
 
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/signup', {
+      const res = await axios.post('http://${process.env.ip}:5000/api/auth/signup', {
         name,
         email,
         password
