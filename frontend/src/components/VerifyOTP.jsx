56ef030d4b6377c4eb74950c1d0606449aa0210d
sraj9506 <154707773+sraj9506@users.noreply.github.com>
2025-01-13 23:52:20 +0530

    Update VerifyOTP.jsx

--- frontend/src/components/VerifyOTP.jsx
+++ frontend/src/components/VerifyOTP.jsx
@@ -41,7 +41,7 @@ const VerifyOTP = () => {
     }
 
     try {
-      const res = await axios.post(`http://localhost:5000/api/auth/verify-otp`, { userId, otp });
+      const res = await axios.post(`http://43.205.236.112:5000/api/auth/verify-otp`, { userId, otp });
       alert(res.data.message);
       navigate('/signin'); // Redirect to login page
     } catch (error) {
